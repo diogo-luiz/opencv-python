1ae756da496a28dbbdcbf778620bd5496b26c466
Diogo <profissional.diogoribeiro@gmail.com>
2024-01-26 23:55:27 -0300

    Testing face detection

---
+++ BasicFaceDetection.py
@@ -0,0 +1,23 @@
+import cv2
+import mediapipe as mp
+
+webcam = cv2.VideoCapture(0)
+face_cascade = mp.solutions.face_detection
+face_reco = face_cascade.FaceDetection()
+draw = mp.solutions.drawing_utils
+
+while True:
+    verify, frame = webcam.read()
+    if not verify:
+        break
+
+    face_list = face_reco.process(frame)
+
+    if face_list.detections:
+        for face in face_list.detections:
+            draw.draw_detection(frame, face)
+    
+    cv2.imshow("Face Reco", frame)
+    if cv2.waitKey(5) == 27:
+        break
+webcam.release()
